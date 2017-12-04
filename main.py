c553591389c269371c3321512f9638e7ce9fbd9d
harsgak <harsgak@yahoo.com>
2017-12-04 23:46:58 +0530

    Subtracting thresholded images to get fly silhouette.
    +Subtracting thresholded currentframe from  thresholded firstframe and applying a medianfilter to get pure fly silhouette.
    ~Formatting changes

--- main.py
+++ main.py
@@ -7,20 +7,44 @@ with open('user_config.ini') as infile:
     video_path = user_config['source_video_path']
 
 cap = cv2.VideoCapture(video_path)
+ret, firstframe = cap.read()
 
+#Run all filters on first image
+firstgray  = cv2.cvtColor(firstframe, cv2.COLOR_BGR2GRAY)
+firstfilt1 = cv2.bilateralFilter(firstgray,7,75,75)
+firstfilt2 = cv2.medianBlur(firstfilt1,7)
+ret, firstthresh = cv2.threshold(firstfilt2,127,255,cv2.THRESH_BINARY)
+firstedges = cv2.Canny(firstthresh,75,75)
+
+#Loop over video
 while(cap.isOpened()):
     ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    filt1 = cv2.bilateralFilter(gray,7,75,75)
-    filt2 = cv2.medianBlur(filt1,7)
-    #gray = cv2.GaussianBlur(gray,(3,3),0)
-    ret1, thresh = cv2.threshold(filt2,127,255,cv2.THRESH_BINARY)
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('gray',gray)
+    
+    frame = cv2.medianBlur(gray,5)
+    #cv2.imshow('medianBlur',frame)
+    
+    frame = cv2.bilateralFilter(frame,7,75,75)
+    #cv2.imshow('bfilter',frame)
+    #filt2 = cv2.GaussianBlur(frame,(5,5),2)
+    
+    ret, thresh = cv2.threshold(frame,127,255,cv2.THRESH_BINARY)
+    cv2.imshow('thresh',thresh)
+
     #laplacian = cv2.Laplacian(denoise,cv2.CV_64F)
     edges = cv2.Canny(thresh,75,75)
+    cv2.imshow('edges',edges)
+    
+    threshdiff = firstthresh - thresh
+    #threshdiff = cv2.absdiff(firstthresh, frame)
+    #cv2.imshow('threshdiff',threshdiff)
+    
+    threshdiffblur = cv2.medianBlur(threshdiff,5)
+    cv2.imshow('fly_silhouette',threshdiffblur)
+    
 
-    cv2.imshow('frame',gray)
-    cv2.imshow('frame1',edges)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
